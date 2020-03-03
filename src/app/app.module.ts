bea73c225f18ae0309c83a418bd705b131736bb5
CienZZZ <Krzysztofkan@poczta.fm>
2020-03-03 10:48:08 +0100

    added home and company components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CompanyComponent } from './company/company.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule
